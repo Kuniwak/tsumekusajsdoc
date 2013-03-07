0189bc7bad6a0eecd59dfccc4aff5240d1dceb55
OrgaChem <orga.chem.job@gmail.com>
2013-03-08 02:38:11 +0900

    Remove doclet wrapper module for class, namespace

--- tsumekusaJsdoc/documents/DocletWrapper.js
+++ tsumekusaJsdoc/documents/DocletWrapper.js
@@ -4,7 +4,8 @@
 
 
 /**
- * An abstract class for doclet wrapper.  This wrapper has mem
+ * A class for doclet wrapper.  This wrapper stores static/instance members
+ * additionally.
  * @param {jsdoc.Doclet} doclet Doclet to be wrapped.
  * @constructor
  */
@@ -12,7 +13,40 @@ var DocletWrapper = function(opt_doclet) {
   if (opt_doclet) {
     this.setOriginalDoclet(opt_doclet);
   }
+
+  this.staticMethods = [];
+  this.staticProperties = [];
+  this.instanceMethods = [];
+  this.instanceProperties = [];
 };
+
+
+/**
+ * An array of static methods.
+ * @type {Array.<jsdoc.Doclet>}
+ */
+DocletWrapper.prototype.staticMethods = null;
+
+
+/**
+ * An array of static properties.
+ * @type {Array.<jsdoc.Doclet>}
+ */
+DocletWrapper.prototype.staticProperties = null;
+
+
+/**
+ * An array of instance methods.
+ * @type {Array.<jsdoc.Doclet>}
+ */
+DocletWrapper.prototype.instanceMethods = null;
+
+
+/**
+ * An array of instance properties.
+ * @type {Array.<jsdoc.Doclet>}
+ */
+DocletWrapper.prototype.instanceProperties = null;
 
 
 /**
@@ -37,6 +71,47 @@ DocletWrapper.prototype.setOriginalDoclet = function(doclet) {
  */
 DocletWrapper.prototype.getOriginalDoclet = function() {
   return this.doclet_;
+};
+
+
+/**
+ * Appends a doclet of static method.  This method is chainable.
+ * @param {jsdoc.Doclet} doclet Doclet of static method.
+ * @param {jsdoc.structs.DocletWrapper} This instance.
+ */
+DocletWrapper.prototype.appendStaticMethod = function(doclet) {
+  this.staticMethods.push(doclet);
+  return this;
+};
+
+
+/**
+ * Appends a doclet of instance method.  This method is chainable.
+ * @param {jsdoc.Doclet} doclet Doclet of instance method.
+ * @param {jsdoc.structs.DocletWrapper} This instance.
+ */
+DocletWrapper.prototype.appendInstanceMethod = function(doclet) {
+  this.instanceMethods.push(doclet);
+};
+
+
+/**
+ * Appends a doclet of static property.  This method is chainable.
+ * @param {jsdoc.Doclet} doclet Doclet of static property.
+ * @param {jsdoc.structs.DocletWrapper} This instance.
+ */
+DocletWrapper.prototype.appendStaticProperty = function(doclet) {
+  this.staticProperties.push(doclet);
+};
+
+
+/**
+ * Appends a doclet of static property.  This method is chainable.
+ * @param {jsdoc.Doclet} doclet Doclet of static property.
+ * @param {jsdoc.structs.DocletWrapper} This instance.
+ */
+DocletWrapper.prototype.appendInstanceProperty = function(doclet) {
+  this.instanceProperties.push(doclet);
 };
 
 
