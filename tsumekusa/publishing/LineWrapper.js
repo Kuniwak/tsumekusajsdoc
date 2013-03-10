c6444d347f1b73d9529a484810c45dd749ec35b6
OrgaChem <orga.chem.job@gmail.com>
2013-03-11 02:32:18 +0900

    Fix a bug on line wrapper

--- tsumekusa/publishing/LineWrapper.js
+++ tsumekusa/publishing/LineWrapper.js
@@ -49,98 +49,123 @@ LineWrapper.prototype.splitWords = function(contents) {
 
 /**
  * Wraps contents with loose hyphenation.
- * NOTE: The method may hyphenate in a content that do not allow break in.
+ * NOTE: The method may hyphenate in a content that do not allow break in when
+ * the content is longer than given text width..
  * @param {Array.<tsumekusa.contents.InlineContent>} contents Contents to wrap.
  * @param {number} width Text width.
- * @param {?number=} opt_indent Optional indent width.
+ * @param {?tsumekusa.publishing.LineWrapper.Indent=} opt_indent Optional
+ *     indent.  No indent if undefined.
  * @return {string} Wrapped string.
  */
 LineWrapper.prototype.wrap = function(contents, width, opt_indent) {
-  var indentWidth = opt_indent || 0;
-  var indentWhites = indentWidth > 0 ? string.repeat(' ', indentWidth) : '';
-  var textWidth = width - indentWidth;
+  var indent = opt_indent || new LineWrapper.Indent();
+  var white = ' ';
 
   var words = this.splitWords(contents);
-  var lineEnd = '\n' + indentWhites;
 
   if (width <= 0) {
     throw Error('Width is too shorter: ' + width);
   }
 
-  if (indentWidth < 0) {
-    throw Error('Indent width is too shorter: ' + indentWidth);
-  }
-
-  if (textWidth < 0) {
-    throw Error('Indent width is longer than width: ' + indentWidth + ' > ' +
-                textWidth);
-  }
-
-  // set first indent.
-  var arr = [indentWhites], arrIndex = 1;
+  // lineLen is excluded indent width.
   var lineLen = 0;
+  var lineIdx = 0;
+  var whites = string.repeat(white, indent.getIndentWidth(lineIdx));
+  // set first indent.
+  var arr = [whites], arrIdx = 1;
 
-  words.forEach(function(word, index) {
+  var indentWidth;
+  var textWidth;
+
+  var appendBreak = function() {
+    indentWidth = indent.getIndentWidth(++lineIdx);
+    whites = string.repeat(white, indentWidth);
+    arr[arrIdx++] = '\n' + whites;
+    textWidth = width - indentWidth;
+    if (textWidth <= 0) {
+      throw Error('Indent width is greater than a width: ' + indentWidth +
+                  '>=' + width);
+    }
+    lineLen = 0;
+  };
+
+  var appendWhite = function() {
+    arr[arrIdx++] = white;
+    lineLen += 1;
+  };
+
+  var appendWord = function(word) {
+    arr[arrIdx++] = word;
+    lineLen += word.length;
+  };
+
+  words.forEach(function(word, idx) {
     var wordLen = word.length;
-    var remainder = textWidth > lineLen ? textWidth - lineLen - 1 : 0;
+    indentWidth = indent.getIndentWidth(lineIdx);
+    textWidth = width - indentWidth;
 
-    if (wordLen <= remainder) {
-      if (index > 0) {
-        arr[arrIndex++] = ' ';
-        lineLen += 1;
+    // Whether hyphenation is necessary.
+    if (wordLen > textWidth) {
+      // Whether hyphen is insertable at last. A hyphen is not insertable when a
+      // remainder is less than 3 chars (a white, a char, a hyphen).
+      if (textWidth - lineLen < 3) {
+        appendBreak();
       }
-      arr[arrIndex++] = word;
-      lineLen += wordLen;
-      return;
-    }
+      else {
+        appendWhite();
+      }
+      var start = 0;
+      var end = textWidth - lineLen;
 
-    // do NOT hyphenate if the word length is lower than text width.
-    if (wordLen <= textWidth) {
-      arr[arrIndex++] = lineEnd;
-      arr[arrIndex++] = word;
-      lineLen = wordLen;
-      return;
-    }
+      while (end < wordLen) {
+        end -= 1; // 1 is a hyphen width.
+        appendWord(word.slice(start, end) + '-');
+        appendBreak();
+        start = end;
+        end = start + textWidth;
+      }
 
-    // force break, if the word is larger than the text width.
-    var start, end, last;
-
-    // subtract a hyphen length from remainder
-    remainder -= 1;
-
-    if (remainder <= 0) {
-      arr[arrIndex++] = lineEnd;
-      arr[arrIndex++] = word.slice(0, textWidth - 1);
-      start = textWidth - 1;
+      appendWord(word.slice(start, wordLen));
     }
     else {
-      arr[arrIndex++] = ' ';
-      arr[arrIndex++] = word.slice(0, remainder);
-      start = remainder;
+      // Whether a word exists before the word.
+      if (idx > 0) {
+        // Whether a word and a white fall inside within the text width.
+        if (wordLen + 1 <= textWidth - lineLen) {
+          appendWhite();
+        }
+        // Whether a line break is necessary.
+        else {
+          appendBreak();
+        }
+      }
+      appendWord(word);
     }
-
-    // check hyphenation will be done next
-    if (wordLen - start <= textWidth) {
-      end = start + textWidth;
-    }
-    else {
-      end = start + textWidth - 1;
-    }
-
-    // force break continually until word end was got.
-    while (start < wordLen) {
-      arr[arrIndex++] = '-';
-      arr[arrIndex++] = lineEnd;
-      last = arr[arrIndex++] = word.slice(start, end);
-      start = end;
-      end += textWidth - 1;
-    }
-
-    lineLen = last.length;
   });
 
-  return arr.join('');
+  return string.trimRight(arr.join(''));
 };
+
+
+
+/**
+ * A class for indent for {@link tsumekusa.publishing.LineWrapper#wrap}.
+ * @constructor
+ */
+LineWrapper.Indent = function(opt_indentWidth) {
+  this.indentWidth_ = opt_indentWidth || 0;
+};
+
+
+/**
+ * Returns indent width by line index.
+ * @param {number} lineIdx Line index that is indent insert before.
+ * @return {number} Indent width.
+ */
+LineWrapper.Indent.prototype.getIndentWidth = function(lineIdx) {
+  return this.indentWidth_;
+};
+
 
 
 // Exports the constructor
---
+++ tsumekusa/publishing/test_LineWrapper.js
@@ -0,0 +1,32 @@
+// This script licensed under the MIT.
+// http://orgachem.mit-license.org
+
+var LOREM_IPSUM = ['Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'];
+
+var LineWrapper = require('./LineWrapper');
+
+exports.testWrap = function(test) {
+  var wrapper = new LineWrapper();
+  var wrapped;
+
+  // General condition
+  wrapped = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor\nincididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis\nnostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu\nfugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in\nculpa qui officia deserunt mollit anim id est laborum.';
+ 
+  test.equal(wrapper.wrap(LOREM_IPSUM, 80), wrapped);
+
+  // Hard condition
+  wrapped = 'Lorem\nipsum\ndolor\nsit\namet,\ncons-\necte-\ntur\nadip-\nisic-\ning\nelit,\nsed\ndo e-\niusm-\nod t-\nempor\ninci-\ndidu-\nnt ut\nlabo-\nre et\ndolo-\nre\nmagna\naliq-\nua.\nUt\nenim\nad\nminim\nveni-\nam,\nquis\nnost-\nrud\nexer-\ncita-\ntion\nulla-\nmco\nlabo-\nris\nnisi\nut a-\nliqu-\nip ex\nea c-\nommo-\ndo c-\nonse-\nquat.\nDuis\naute\nirure\ndolor\nin r-\nepre-\nhend-\nerit\nin v-\nolup-\ntate\nvelit\nesse\ncill-\num d-\nolore\neu f-\nugiat\nnulla\npari-\natur.\nExce-\npteur\nsint\nocca-\necat\ncupi-\ndatat\nnon\nproi-\ndent,\nsunt\nin\nculpa\nqui\noffi-\ncia\ndese-\nrunt\nmoll-\nit\nanim\nid\nest\nlabo-\nrum.';
+
+  test.equal(wrapper.wrap(LOREM_IPSUM, 5), wrapped);
+
+  var indent = new LineWrapper.Indent();
+  indent.getIndentWidth = function(lineIdx) {
+    return lineIdx;
+  };
+
+  // Indent test.
+  wrapped = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit,\n sed do eiusmod tempor incididunt ut labore et dolore magna\n  aliqua. Ut enim ad minim veniam, quis nostrud exercitation\n   ullamco laboris nisi ut aliquip ex ea commodo consequat.\n    Duis aute irure dolor in reprehenderit in voluptate\n     velit esse cillum dolore eu fugiat nulla pariatur.\n      Excepteur sint occaecat cupidatat non proident, sunt\n       in culpa qui officia deserunt mollit anim id est\n        laborum.';
+
+  test.equal(wrapper.wrap(LOREM_IPSUM, 60, indent), wrapped);
+  test.done();
+};
