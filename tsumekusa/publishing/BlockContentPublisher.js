19a2cdab044afd5a8013c499fb5984af371dbe3d
OrgaChem <orga.chem.job@gmail.com>
2013-03-15 05:52:47 +0900

    Improve indentation argorithm

--- tsumekusa/publishing/BlockContentPublisher.js
+++ tsumekusa/publishing/BlockContentPublisher.js
@@ -2,6 +2,7 @@
 // http://orgachem.mit-license.org
 
 
+
 /**
  * An abstract class for block content publisher.
  * @constructor
@@ -11,12 +12,99 @@ var BlockContentPublisher = function() {};
 
 
 /**
- * Returns an indent level of the content.
- * @param {tsumekusa.contents.BlockContent} content Block content.
+ * Display width.  Usually the property is used in a console.
+ * @type {?number}
+ * @private
  */
-BlockContentPublisher.prototype.getIndentLevel = function(content) {
-  var parent = content.getParent();
-  return parent ? parent.getPublisher().getIndentLevel(parent) : 0;
+BlockContentPublisher.prototype.dispWidth_ = Number.MAX_VALUE;
+
+
+/**
+ * Sets a display width.  The setter mostly used in a CUI environment.
+ * @param {number} width Display width.
+ */
+BlockContentPublisher.prototype.setDisplayWidth = function(width) {
+  this.dispWidth_ = width;
+};
+
+
+/**
+ * Returns a display width.
+ * @return {number} Display width.
+ */
+BlockContentPublisher.prototype.getDisplayWidth = function() {
+  return this.dispWidth_;
+};
+
+
+/**
+ * Returns an indent width of the content.
+ * @param {tsumekusa.contents.BlockContent} content Block content to get an
+ *     indent width.
+ * @return {number} Indent width.
+ */
+BlockContentPublisher.prototype.getIndentWidth = function(content) {
+  var parent, indentWidth;
+  if (parent = content.getParent()) {
+    var publisher = parent.getPublisher();
+    if (!publisher) {
+      throw Error('Illegal publisher was given: ' + publisher);
+    }
+    indentWidth = publisher.getIndentWidth(parent);
+  }
+  else {
+    indentWidth = 0;
+  }
+
+  if (indentWidth < 0) {
+    throw Error('Indent width have to be greater than 0, but come: ' +
+        indentWidth);
+  }
+
+  return this.getIndentWidthInternal(content, indentWidth);
+};
+
+
+/**
+ * Returns an indent width of the content.
+ * @param {tsumekusa.contents.BlockContent} content Block content to get an
+ *     indent width.
+ * @param {number} width Indent width of a content as a parent of the {@code
+ *     content}.  It is 0, if the parent is not defined.
+ * @return {number} Indent width of the {@code content}.
+ * @protected
+ */
+BlockContentPublisher.prototype.getIndentWidthInternal = function(content, width) {
+  return width;
+};
+
+
+/**
+ * Returns an object for indentation.  Override the method if you want to change
+ * a strategy to indent.  In defaultm constant width indentation by {@link
+ * #getIndentWidth}.
+ * @param {tsumekusa.contents.BlockContent} content Block content to get an
+ *     object for indentation.
+ * @return {tsumekusa.publishing.WordWrapper.Indent} Created object for
+ *     indentation.
+ * @protected
+ */
+BlockContentPublisher.prototype.getIndent = function(content) {
+  return new WordWrapper.Indent(this.getIndentWidth(content));
+};
+
+
+/**
+ * Returns a word wrapper.  Override the method if you want to change
+ * a strategy to wrap words.
+ * @param {tsumekusa.contents.BlockContent} content Block content to get a word
+ *     wrapper.
+ * @return {tsumekusa.publishing.WordWrapper.Indent} Created object for
+ *     indentation.
+ * @protected
+ */
+BlockContentPublisher.prototype.getWordWrapper = function(content) {
+  return new WordWrapper(this.getDisplatWidth(), this.getIndent(content));
 };
 
 
