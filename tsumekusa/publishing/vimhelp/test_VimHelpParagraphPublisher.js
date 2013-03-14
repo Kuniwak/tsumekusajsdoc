19de8c5834e1abe3a59788ceb0f828aa23935dc0
OrgaChem <orga.chem.job@gmail.com>
2013-03-15 07:37:50 +0900

    Add test file

---
+++ tsumekusa/publishing/vimhelp/test_VimHelpParagraphPublisher.js
@@ -0,0 +1,26 @@
+// This script licensed under the MIT.
+// http://orgachem.mit-license.org
+
+
+var basePath = '../../../tsumekusa';
+var Paragraph = require(basePath + '/contents/Paragraph');
+var VimHelpParagraphPublisher = require(basePath +
+    '/publishing/vimhelp/VimHelpParagraphPublisher');
+
+var publisher = new VimHelpParagraphPublisher();
+
+exports.testPublish = function(test) {
+  var p = new Paragraph('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+
+  var CORRECT = [
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
+    'tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,',
+    'quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo',
+    'consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse',
+    'cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non',
+    'proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  ].join('\n');
+
+  test.equal(publisher.publish(p), CORRECT);
+  test.done();
+};
