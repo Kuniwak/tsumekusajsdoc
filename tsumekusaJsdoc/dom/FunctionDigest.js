cefe3f542a38dab9842b26f61f1b6696c2611e8d
OrgaChem <orga.chem.job@gmail.com>
2013-03-21 20:03:17 +0900

    Rename MethodDigest to FunctionDigest

--- tsumekusaJsdoc/dom/FunctionDigest.js
+++ tsumekusaJsdoc/dom/FunctionDigest.js
@@ -13,8 +13,8 @@ var Type = require(basePath + '/dom/Type');
 
 
 /**
- * A class for a function digest.  Output example is:
- * {@code foo.bar(arg1, arg2) -> Foo|Bar}
+ * A class for a function digest. Output example is:
+ * {@code foo.bar(arg1, arg2, arg3) => FooBar}
  *
  * @param {jsdoc.Doclet} symbol Function symbol.
  * @param {?tsumekusaJsdoc.dom.DocumentHelper=} opt_docHelper Optional
@@ -36,14 +36,13 @@ FunctionDigest.prototype.publish = function() {
   var digest = [], digestIdx = 0;
   var paramsMax, returnsMax;
 
-  // current detail string as: foobar(
+  // current digest string as: foobar(
   digest[digestIdx++] = symbol.longname + '(';
 
   if (tsumekusaJsdoc.hasParam(symbol)) {
-    // current detail string as: foobar( arg1, arg2, arg3
-    digest[digestIdx++] = symbol.params.map(function(tag) {
-      var paramName = this.createParameterNameString(tag);
-
+    // current digest string as: foobar( arg1, arg2, arg3
+    digest[digestIdx++] = symbol.params.map(function(tag, index) {
+      var paramName = tsumekusaJsdoc.decorateParamName(tag);
       if (FunctionDigest.VERBOSE_PARAM) {
         var type = new Type(tag);
         return paramName + ' ' + type.publish();
@@ -54,43 +53,21 @@ FunctionDigest.prototype.publish = function() {
     }, this).join(', ');
   }
 
-  // current detail string as: foobar( arg1, arg2, arg3 )
+  // current digest string as: foobar( arg1, arg2, arg3 )
   digest[digestIdx++] = ')';
 
   if (tsumekusaJsdoc.hasReturn(symbol)) {
-    // current detail string as: foobar( arg1, arg2, arg3 ) ->
+    // current digest string as: foobar( arg1, arg2, arg3 ) =>
     digest[digestIdx++] = ' -> ';
 
-    // current detail string as: foobar( arg1, arg2, arg3 ) -> val
-    digest[digestIdx++] = symbol.returns.map(function(tag) {
+    // current digest string as: foobar( arg1, arg2, arg3 ) => val
+    digest[digestIdx++] = symbol.returns.map(function(tag, index) {
       var type = new Type(tag);
       return type.publish();
-    }, this).join(', ');
+    }, this);
   }
 
   return digest.join('');
-};
-
-
-/**
- * Creates a parameter name string.
- * @param {jsdoc.Tag} tag Parameter tag.
- * @return {string} Parameter name.
- */
-FunctionDigest.prototype.createParameterNameString = function(tag) {
-  var name = tag.name;
-
-  // display 'foo...' as the tag name if the parameter is variable.
-  if (tag.variable) {
-    name += '...';
-  }
-
-  // display '[foo]' as the tag name if the parameter is optional.
-  if (tag.optional) {
-    name = '[' + name + ']';
-  }
-
-  return name;
 };
 
 
