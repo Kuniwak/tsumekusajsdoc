a459b586c59b295c29c7df0560154011f37fe93c
OrgaChem <orga.chem.job@gmail.com>
2013-03-24 05:20:44 +0900

    Change to no-wrap if the pre is longer than tw

--- tsumekusa/publishing/vimhelp/test/test_VimHelpPreformattedParagraphPublisher.js
+++ tsumekusa/publishing/vimhelp/test/test_VimHelpPreformattedParagraphPublisher.js
@@ -17,18 +17,7 @@ var LOREM_IPSUM = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed
 exports.testPublish = function(test) {
   var p = new PreformattedParagraph(LOREM_IPSUM);
 
-  var CORRECT = [
-    '',
-    'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
-    'tempor incididunt ut labore et dolore magna aliqua.',
-    'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut',
-    'aliquip ex ea commodo consequat.',
-    'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore',
-    'eu fugiat nulla pariatur.',
-    'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia',
-    'deserunt mollit anim id est laborum.',
-    ''
-  ].join('\n');
+  var CORRECT = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
   test.equal(p.publish(), CORRECT);
   test.done();
